f172930a030ea4681233850a404fbe03b1c917e1
ekinchin <evgen4in@gmail.com>
2020-11-08 23:40:32 +0500

    плохой коммит - много мелких изменений:
    - убрана функция parse
    - добавлен пробро данных из запроса от клиента
    - добавлена точка для регистрации
    - изменена логика в роутинге

--- src/api/routing.ts
+++ src/api/routing.ts
@@ -14,11 +14,6 @@ interface httpObject {
     data: string,
   }
 
-const parse = (request: any) => {
-  const { url, headers, body } = request;
-  return { url, headers, body };
-};
-
 const checkAuthorization = async (username: string, password: string)
 : Promise<boolean> => Users.find(username)
   .then((user) => user.password === password)
@@ -36,20 +31,56 @@ const checkSession = async (id: string)
   .catch(() => false);
 
 const route : {[index: string]: any} = {
+  '/api/register': async (request: any, response: any, data: any) => {
+    const { headers } = request;
+    const contentType = headers['content-type'];
+    if (contentType !== 'application/json') {
+      response.statusCode = 400;
+      response.setHeader('Content-Type', 'application/json');
+      response.write(JSON.stringify({ 400: 'not a JSON' }));
+      response.end();
+      return;
+    }
+    console.log(data);
+    let dataJSON: userType;
+    try {
+      dataJSON = JSON.parse(data);
+    } catch (e) {
+      response.statusCode = 400;
+      response.setHeader('Content-Type', 'application/json');
+      response.write(JSON.stringify({ 400: 'JSON error' }));
+      response.end();
+      return;
+    }
+    try {
+      const { username, password } = dataJSON;
+      const user: userType = await Users.add(username, password, ['default']);
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'application/json');
+      response.write(JSON.stringify({ 200: JSON.stringify(user) }));
+      response.end();
+      return;
+    } catch (error) {
+      response.statusCode = 400;
+      response.setHeader('Content-Type', 'application/json');
+      response.write(JSON.stringify({ 400: JSON.stringify(error) }));
+      response.end();
+    }
+  },
+
   '/api/auth': async (request: any, response: any) => {
-    const { headers } = parse(request);
+    const { headers } = request;
     const { username, password, session } = headers;
 
     if (session) {
-      Sessions.find(session)
-        .then((client) => Sessions.delete(client.id))
+      Sessions.delete(session)
         .catch(() => {});
     }
     const isAuth = username && password ? await checkAuthorization(username, password) : false;
     if (!isAuth) {
       response.statusCode = 401;
       response.setHeader('Content-Type', 'application/json');
-      response.write(JSON.stringify({ 401: '' }));
+      response.write(JSON.stringify({ 401: 'authorization error' }));
     } else {
       const client = await Sessions.add(username);
       response.statusCode = 400;
@@ -58,8 +89,9 @@ const route : {[index: string]: any} = {
     }
     response.end();
   },
+
   '/api/unauth': (request: any, response: any) => {
-    const { headers } = parse(request);
+    const { headers } = request;
     const { session } = headers;
     Sessions.find(session)
       .then((client) => Sessions.delete(client.id))
@@ -67,25 +99,41 @@ const route : {[index: string]: any} = {
     response.statusCode = 200;
     response.end();
   },
+
   '/api/notfound': (request: any, response: any) => {
     response.statusCode = 404;
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify({ 404: 'endpoint not exist' }));
+    response.end();
+  },
+
+  '/api/unauthorization': (request: any, response: any) => {
+    const { headers } = request;
+    const { session } = headers;
+    if (session) {
+      Sessions.find(session)
+        .then((client) => Sessions.delete(client.id))
+        .catch(() => {});
+    }
+    response.statusCode = 401;
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify({ 401: 'access denied - unauthorization' }));
     response.end();
   },
 };
 
-const routing = async (request: any, response: any): Promise<any> => {
-  const { url, headers } = parse(request);
+const routing = async (request: any, response: any, data: any): Promise<any> => {
+  const { url, headers } = request;
   const { session } = headers;
   // определение наличия валидной сессии
   const sessionIsValid: boolean = await checkSession(session);
-  if (sessionIsValid || url === '/api/auth') {
-    // eslint-disable-next-line no-prototype-builtins
-    const endpoint = route.hasOwnProperty(url) ? url : '/api/notfound';
-    route[endpoint](request, response);
+  let endpoint: string = '';
+  if (!sessionIsValid && url !== '/api/register' && url !== '/api/auth') {
+    endpoint = '/api/unauthorization';
   } else {
-    response.writeHead(301, { Location: 'http://127.0.0.1:8000/api/auth' });
-    response.end();
+    endpoint = route.hasOwnProperty(url) ? url : '/api/notfound';
   }
+  route[endpoint](request, response, data);
 };
 
 export default routing;
