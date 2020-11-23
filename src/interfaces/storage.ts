67f3820643fcb02ef4f584af452b77e2ef1ae08a
ekinchin <evgen4in@gmail.com>
2020-11-23 22:32:32 +0500

    FIX: в интерфейсе нельзя описывать конструктор

--- src/interfaces/storage.ts
+++ src/interfaces/storage.ts
@@ -1,5 +1,4 @@
 export interface IStorage {
-  constructor: (database: string) => string,
   create:(document: string) => Promise<string>,
   read: (field: string, value: string) => Promise<string>,
   update: (field: string, value: string, document: string) => Promise<string>,
