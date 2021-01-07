4a3062a217b7f027f9aacfc8e42a8b22d9a351c0
ekinchin <evgen4in@gmail.com>
2021-01-07 17:27:49 +0500

    инициализация БД. инициализация Сущностей. инициализация WebControlle'a. инициализация API.
    Необходимые сущности передаются в API и WebController. //внедрение зависимостей через аргументы фабричных функций.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
+import Storage from './storage';
+import { UsersClass } from './entities/users';
+import { SessionsClass } from './entities/sessions';
+// eslint-disable-next-line no-unused-vars
+import type { TUser, TSession } from './types';
 import createServer from './server';
-import api from './api';
-import WebController from './controllers/WebController';
+import createApi from './api';
+import createWebController from './controllers/WebController';
 
-console.log('Running...');
-createServer(WebController(api));
+const usersDatabase = new Storage<TUser>('User');
+const Users = new UsersClass(usersDatabase);
+
+const sessionsDatabase = new Storage<TSession>('Session');
+const Sessions = new SessionsClass(sessionsDatabase);
+
+const api = createApi(Sessions, Users);
+const WebController = createWebController(api, Sessions);
+const webServer = createServer(WebController);
+
+export { Users, Sessions };
