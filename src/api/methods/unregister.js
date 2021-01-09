5add01e4b480d8a5d1146b4ec8f830594fe7c531
ekinchin <evgen4in@gmail.com>
2021-01-10 00:59:52 +0500

    реализовано удаление пользователей.
    необходимы корректная сессия и учетные данные.

--- src/api/methods/unregister.js
+++ src/api/methods/unregister.js
@@ -1,21 +1,27 @@
 const unregister = async (request) => {
-  const { data } = request;
-  const { username } = data ? JSON.parse(data) : undefined;
-  const result = await Users.delete(username);
-  if (result) {
-    return {
-      status: {
-        code: 0,
-        message: '',
-      },
-      data: JSON.stringify({ user: result }),
-    };
+  const { data, session } = request;
+  const { username, password } = data ? JSON.parse(data) : undefined;
+  const findedSession = await Sessions.find('_id', session);
+  const findedUser = await Users.find('_id', findedSession.result[0].userId);
+  if (username === findedUser.result[0].username && password === findedUser.result[0].password) {
+    const deleted = await Users.delete(username);
+    const { result } = deleted;
+    if (result) {
+      return {
+        status: {
+          code: 0,
+          message: '',
+        },
+        data: { username: result.username },
+      };
+    }
   }
   return {
     status: {
       code: 1,
       message: '',
     },
+    data: { error: 'your token !== your username' },
   };
 };
 
