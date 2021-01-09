2742f4c48025081a2d3aef2ae791b97a2bd3121a
ekinchin <evgen4in@gmail.com>
2021-01-10 00:07:41 +0500

    реализован логаут с удалением сессии

--- src/api/methods/unauth.js
+++ src/api/methods/unauth.js
@@ -1,11 +1,16 @@
 // eslint-disable-next-line no-unused-vars
-const unauth = async (request) => ({
-  status: {
-    code: 1,
-    message: 'Unauthorized',
-  },
-  data: JSON.stringify({ error: 'Unauthorized' }),
-});
+const unauth = async (request) => {
+  const { session } = request;
+  const deleted = await Sessions.delete(session);
+  const { result } = deleted;
+  return {
+    status: {
+      code: 0,
+      message: 'OK',
+    },
+    data: { session: result.id },
+  };
+};
 
 // eslint-disable-next-line func-names
 (function () {
