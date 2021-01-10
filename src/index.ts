a86fce78bb6beb3610e424efe0affc6ef194988d
ekinchin <evgen4in@gmail.com>
2021-01-10 22:04:57 +0500

    инициализация сущности Notes, передача ее в createApi

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@
 import Storage from './storage';
 import { UsersClass } from './entities/users';
 import { SessionsClass } from './entities/sessions';
+import { NotesClass } from './entities/notes';
 // eslint-disable-next-line no-unused-vars
-import type { TUser, TSession } from './types';
+import type { TUser, TSession, TNote } from './types';
 import createServer from './server';
 import createApi from './api';
 import createWebController from './controllers/WebController';
@@ -15,7 +16,10 @@ const Users = new UsersClass(usersDatabase);
 const sessionsDatabase = new Storage<TSession>('Session');
 const Sessions = new SessionsClass(sessionsDatabase);
 
-const api = createApi(Sessions, Users);
+const notesDatabase = new Storage<TNote>('Note');
+const Notes = new NotesClass(notesDatabase);
+
+const api = createApi(Sessions, Users, Notes);
 const WebController = createWebController(api, Sessions);
 // eslint-disable-next-line no-unused-vars
 const webServer = createServer(WebController);
