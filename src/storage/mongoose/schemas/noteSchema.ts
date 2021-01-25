91e1fe20900ae0f02797131c44c4ba75974fa65e
ekinchin <evgen4in@gmail.com>
2021-01-25 23:38:29 +0500

    FIX: id записи должен быть числом - меткой времени.

--- src/storage/mongoose/schemas/noteSchema.ts
+++ src/storage/mongoose/schemas/noteSchema.ts
@@ -2,6 +2,7 @@
 import { Schema } from 'mongoose';
 // eslint-disable-next-line import/prefer-default-export
 export const noteSchema = new Schema({
+  id: Number,
   date: String,
   userId: String,
   text: String,
